0a5eccd7860e5dfa5c49ba41a1ed9b96fc92dd0a
sahihyti <sahithi@gmail.com>
2026-04-20 11:04:36 +0530

    adding githubaction

--- basicprograms/findoccuranceofnumberinalist.ts
+++ basicprograms/findoccuranceofnumberinalist.ts
@@ -3,20 +3,18 @@ const a: number[] = [11, 10, 2, 3, 4, 5, 6, 4, 7, 8, 9, 11, 22, 4, 5, 6, 8, 9];
 const b: { [key: number]: number } = {};
 
 // Count frequency
-for (let i = 0; i < a.length; i++) {
-  const num = a[i];
+for (let i of a) {
 
-  if (num in b) {
-    b[num] = b[num] + 1;
+
+  if (i in b) {
+    b[i] = b[i] + 1;
   } else {
-    b[num] = 1;
+    b[i] = 1;
   }
 }
 
 console.log(b);
 
-
-// Print like Python: value : key
 for (const key in b) {
   const value = b[key];
   console.log( key +" : "+ value + " , ");
